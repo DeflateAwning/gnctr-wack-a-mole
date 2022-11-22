82123e292d2fb2f55e613da1d1710b31e641e512
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2022-11-21 21:33:42 -0700

    Integrate tutorial code with testing framework

--- gnctr_wackamole_firmware/gnctr_wackamole_firmware.ino
+++ gnctr_wackamole_firmware/gnctr_wackamole_firmware.ino
@@ -18,6 +18,36 @@ TM1637Display seg3(PIN_SEG3_CLK, PIN_SEG3_DIO);
 #define seg_cur_score (seg1)
 #define seg_hi_score (seg3)
 
+#define START_SWITCH_NUM 9 // switch number of Start button
+
+
+const int startLitTime = 3000; // How long Moles Stay Lit at Start of Games
+const int Level2000ms = 50; // Score when Moles Lit Time changes to 2 seconds
+const int Level1000ms = 200; // Score when Moles Lit Time changes to 1 second
+const int Level500ms = 400; // Score when Moles Lit Time changes to half a second
+const int MoleLevel2 = 100; // Score when number of Moles at a time changes from 1 to 2
+const int MoleLevel3 = 300; // Score when number of Moles at a time changes from 2 to 3
+
+int buttonState[8] = { 0, 0, 0, 0, 0, 0, 0, 0 }; // State of Input Switches to detect whe button is pressed
+int lastButtonState[8] = { 0, 0, 0, 0, 0, 0, 0, 0 }; // State of button last time looped.
+
+int startButtonState = 0;
+boolean moleActive[8] = { false, false, false, false, false, false, false, false }; // Indicator if Mole should be active
+long lastPressed[8]; // Time of when key was last pressed. Used to eliminate stuttering when switch contact is made
+long moleStart[8]; // Start of mole lit time
+long moleEnd[8]; // Expiry timeo mole's lit time
+boolean Started = false;
+boolean Finished;
+int molesLit = 0; // Number of moles lit
+int score = 0;
+int hiScore = 100;
+int newMole;
+long startTimer;
+int timeToPlay = 30;
+long idleStart; // Used to calculate how long since idle on start up
+boolean bonusUsed = false;
+
+
 int get_switch_pin_number(int switch_num) {
   // switch_num is a number from 1 to 10
   
@@ -34,7 +64,7 @@ int get_light_pin_number(int light_num) {
 
 bool is_switch_pressed(int switch_num) {
   // returns true if pressed
-  return digitalRead(get_switch_pin_number(switch_num));
+  return ! digitalRead(get_switch_pin_number(switch_num));
 }
 
 void set_light_status(int light_num, int on_or_off) {
@@ -69,6 +99,12 @@ void setup() {
   // Init 7-segs, and set to all on
   seg1.setBrightness(0x0f);
   seg3.setBrightness(0x0f);
+
+  // Run Test
+  run_test();
+
+  // Game Logic
+  idleStart = millis();
   
 
   
@@ -108,8 +144,8 @@ void run_test() {
     delay(750);
   }
 
-  for (int i = 0; i < 30; i++) {
-    Serial.println("Reading switch states for 30 sec.");
+  for (int i = 0; i < 12; i++) {
+    Serial.println("Reading switch states for 12 sec.");
     for (int switch_num = 1; switch_num <= 10; switch_num++) {
       Serial.print("Switch "); Serial.print(switch_num); Serial.print(" = ");
       Serial.println(is_switch_pressed(switch_num));
@@ -119,6 +155,260 @@ void run_test() {
 }
 
 void loop() {
-  run_test();
+  game_loop();
 
 }
+
+
+void game_loop() {
+    int secs;
+    int i8;
+    int i600;
+    secs = int((millis() - idleStart) / 1000);
+    i8 = secs - (8 * (int(secs / 8))); // 0 to 7 depending on time since idle
+    if (i8 <= 3) {
+        // 0 to 3
+        seg_cur_score.showNumberDec(hiScore, false);
+        seg_hi_score.showNumberDec(hiScore, false);
+        // write display
+        // seg_cur_score.blinkRate(2);
+    } else {
+        // 4 to 7
+        UpdateScore();
+        // seg_cur_score.blinkRate(0);
+    }
+    if (Started) {
+        StartGame();
+        Started = false;
+    }
+    startButtonState = is_switch_pressed(START_SWITCH_NUM);
+    // Serial.println(startButtonState);
+    if (startButtonState == 1) {
+        Started = true;
+    }
+    i600 = secs - (600 * (int(secs / 600))); // every 10 minutes
+    // Serial.println(i600);
+    if (i600 == 599) {
+        // then idle for 10 minutes
+        // so make a noise to attract attention
+        // DUM 1
+        set_light_status(0+1, 1);
+        delay(10);
+        set_light_status(0+1, 0);
+        delay(150);
+        // DIDDY 2 3
+        set_light_status(2+1, 1);
+        delay(10);
+        set_light_status(2+1, 0);
+        delay(350);
+        set_light_status(1+1, 1);
+        delay(10);
+        set_light_status(1+1, 0);
+        delay(250);
+        // DUMDUM 4 5
+        set_light_status(5+1, 1);
+        delay(10);
+        set_light_status(5+1, 0);
+        delay(250);
+        set_light_status(7+1, 1);
+        delay(10);
+        set_light_status(7+1, 0);
+        delay(650);
+        // DUM DUM 6 7
+        set_light_status(6+1, 1);
+        delay(10);
+        set_light_status(6+1, 0);
+        delay(400);
+        set_light_status(6+1, 1);
+        delay(10);
+        set_light_status(6+1, 0);
+        delay(3000);
+    }
+}
+
+void StartGame() {
+    int i;
+    int timeLeft;
+    long nowtime;
+    long keytime;
+    long startTimeLeft;
+    randomSeed(analogRead(0));
+    // seg_cur_score.blinkRate(1);
+    seg_cur_score.showNumberDec(5555, false);
+    // write display
+    delay(1000);
+    seg_cur_score.showNumberDec(4444, false);
+    // write display
+    delay(1000);
+    seg_cur_score.showNumberDec(3333, false);
+    // write display
+    delay(1000);
+    seg_cur_score.showNumberDec(2222, false);
+    // write display
+    delay(1000);
+    seg_cur_score.showNumberDec(1111, false);
+    // write display
+    delay(1000);
+    seg_cur_score.clear();
+    delay(1000);
+    // seg_cur_score.blinkRate(0);
+    molesLit = 0;
+    score = 0;
+    timeToPlay = 30;
+    bonusUsed = false;
+    UpdateScore();
+    Finished = false;
+    Serial.println("Starting Game");
+    for (int i = 0; i <= 7; i++) {
+        buttonState[i] = 0;
+        lastButtonState[i] = 0;
+        moleActive[i] = false;
+    }
+    startTimer = millis();
+    AddMole();
+    do {
+        for (int i = 0; i <= 7; i++) {
+            buttonState[i] = is_switch_pressed(i+1);
+            if (buttonState[i] == 1 && lastButtonState[i] == 0) { // then key has been pressed as state changed from 0 to 1
+                keytime = millis() - lastPressed[i];
+                if (keytime <= 400) { // then ignore as likely jitter\stutter
+                    // do nothing
+                } else {
+                    lastPressed[i] = millis();
+                    Serial.print((String) "Button " + i + " pressed: " + buttonState[i]);
+                    if (moleActive[i] == true) {
+                        score = score + 10;
+                        UpdateScore();
+                        // Set new mole first so current active one not selected;
+                        AddMole();
+                        molesLit = molesLit - 1;
+                        moleActive[i] = 0;
+                        // const MoleLevel2 = 100;
+                        // const MoleLevel3 = 300;
+                        set_light_status(i+1, 0);
+                        if (score >= MoleLevel2 && molesLit <= 1) {
+                            AddMole();
+                        }
+                        if (score >= MoleLevel3 && molesLit <= 2) {
+                            AddMole();
+                        }
+                        // Now check if score over 500 then add 15 seconds
+                        if (bonusUsed == false && score >= 500) {
+                            bonusUsed = true;
+                            timeToPlay = 45;
+                        }
+                    } else {
+                        // Penalty as Mole is not Active when button pressed
+                        score = score - 5;
+                        if (score < 0) {
+                            score = 0;
+                        }
+                        UpdateScore();
+                    } // End Else
+                }
+            }
+            // if (buttonState[i]==0 && lastButtonState[i] == 1)
+            // then button released and not need to deal with that event
+            lastButtonState[i] = buttonState[i];
+        } // end For
+        // Check Status of Moles
+        for (int i = 0; i <= 7; i++) {
+            if (moleActive[i]) {
+                nowtime = millis();
+                if (moleEnd[i] < nowtime) {
+                    Serial.println((String) "Mole " + i + " timed out.");
+                    // Set new mole first so current active one not selected;
+                    AddMole();
+                    molesLit = molesLit - 1;
+                    moleActive[i] = 0;
+                    set_light_status(i+1, 0);
+                }
+            }
+        } // End For
+        timeLeft = timeToPlay - int((millis() - startTimer) / 1000);
+        // Serial.println(timeLeft);
+        if (timeLeft >= 10) {
+            // seg_cur_score.blinkRate(0);
+        } else if (timeLeft >= 6) {
+            // seg_cur_score.blinkRate(1);
+        } else {
+            // seg_cur_score.blinkRate(2);
+        }
+        if (timeLeft <= 0) {
+            Finished = true;
+        }
+        // When game ends, Finished is set to TRUE
+    } while (Finished == false);
+    // turn off all lights at end of game
+    for (int i = 0; i <= 7; i++) {
+        set_light_status(i+1, 0);
+    }
+    // Wait five seconds to show score
+    // seg_cur_score.blinkRate(0);
+    UpdateScore();
+    delay(5000);
+    // seg_cur_score.blinkRate(0);
+    UpdateScore();
+    if (score >= hiScore) {
+        // seg_cur_score.blinkRate(2);
+        hiScore = score;
+        delay(5000);
+    }
+}
+
+
+void AddMole() {
+    int timetolive;
+    timetolive = startLitTime;
+    Serial.println((String) "Choosing new mole. Score= " + score + ". Moles lit=" + molesLit);
+    Serial.println("Choosing new Mole...");
+    ChooseMole();
+    Serial.print("Mole Active: ");
+    Serial.println(newMole);
+    moleActive[newMole] = 1;
+    set_light_status(newMole+1, 1);
+    if (score >= Level2000ms) {
+        timetolive = 2000;
+        if (score >= Level1000ms) {
+            timetolive = 1000;
+            if (score >= Level500ms) {
+                timetolive = 500;
+            }
+        }
+    }
+    moleStart[newMole] = millis();
+    moleEnd[newMole] = millis() + timetolive;
+    molesLit = molesLit + 1;
+}
+
+void ChooseMole() {
+    int findMole;
+    boolean finished;
+    int state;
+    do {
+        finished = false;
+        findMole = random(8);
+        if (moleActive[findMole] == 1) {
+            // Serial.println("Is Zero");
+            finished = false;
+        } else {
+            // Serial.println("Is One");
+            finished = true;
+        }
+        state = is_switch_pressed(findMole+1);
+        if (state == 1) {
+            finished = false;
+        }
+    } while (finished == false);
+    newMole = findMole;
+}
+
+void UpdateScore() {
+    seg_cur_score.showNumberDec(score, false);
+    // write display
+}
+
+void MatrixTest() {
+  Serial.println("Running MatrixTest().");
+}
+
